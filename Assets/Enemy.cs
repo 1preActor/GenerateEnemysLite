3c615ccf481648e9eaa69550b540ac1ec6a76c00
preActor <preactgithub@gmail.com>
2024-09-22 13:51:57 +0300

    init

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,8 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void Start() => transform?.Rotate(0, Random.Range(0,360), 0);
-    
-    private void Update() => transform?.Translate(Vector3.forward*Time.deltaTime);    
+    [SerializeField] private float _speed = 4f;
+
+    private Transform _target;
+
+    private void Update()
+    {
+        transform.LookAt(_target.position);
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
 }
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -6,19 +6,19 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private float _repeatRate;
-  
+
     private void Start() => StartCoroutine(SpawnEnemy());
-    
+
     private IEnumerator SpawnEnemy()
     {
         var wait = new WaitForSeconds(_repeatRate);
-
         while (true)
         {
-            int spawnPointNum = Random.Range(0,_spawnPoints.Length);
-            Instantiate(_enemyPrefab, _spawnPoints[spawnPointNum]);
-
+            _enemyPrefab.SetTarget(_spawnPoints[GetRandomSpawnPoint()]);
+            Instantiate(_enemyPrefab, _spawnPoints[GetRandomSpawnPoint()]);
             yield return wait;
         }
     }
+
+    private int GetRandomSpawnPoint() { return Random.Range(0, _spawnPoints.Length); }
 }
